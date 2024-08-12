232accdd3b27ee42bcec3839753dbafcf49c8291
AlizadaG <elizadegozel@gmail.com>
2024-08-12 09:26:02 +0400

    first

---
+++ CeyhunApplication/ViewComponents/PostViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using CeyhunApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CeyhunApplication.ViewComponents;
+
+public class PostViewComponent : ViewComponent
+{
+    private readonly ApplicationDbContext _context;
+    public PostViewComponent(ApplicationDbContext context) => _context = context;
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        // cd order. top 5  en son eklenen 5 post
+        var posts = await _context.Posts
+            .OrderByDescending(post => post.PublishDate)
+            .Take(5)
+            .ToListAsync();
+        return View(posts);
+    }
+}
